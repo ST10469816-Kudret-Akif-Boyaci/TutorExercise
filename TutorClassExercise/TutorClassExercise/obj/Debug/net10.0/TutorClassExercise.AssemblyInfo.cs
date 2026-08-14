2b30e54beb671e2521e53795d5ed5e9fed3a11b9
ST10469816-Kudret-Akif-Boyaci <st10469816@vcconnect.edu.za>
2026-08-14 11:04:14 +0200

    greeting first commit

--- TutorClassExercise/TutorClassExercise/obj/Debug/net10.0/TutorClassExercise.AssemblyInfo.cs
+++ TutorClassExercise/TutorClassExercise/obj/Debug/net10.0/TutorClassExercise.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TutorClassExercise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e5a55dec68f6f1f57f753de59ff9d26699b04fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("TutorClassExercise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TutorClassExercise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

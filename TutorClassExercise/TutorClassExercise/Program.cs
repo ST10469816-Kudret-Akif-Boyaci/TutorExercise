169fd50ee71b44aa53aee247059e7231e5328ca2
ST10469816-Kudret-Akif-Boyaci <st10469816@vcconnect.edu.za>
2026-08-14 11:06:43 +0200

    farewell push

--- TutorClassExercise/TutorClassExercise/Program.cs
+++ TutorClassExercise/TutorClassExercise/Program.cs
@@ -5,7 +5,11 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+<<<<<<< Updated upstream
             Console.WriteLine("this is a c# console app");
+=======
+            Console.WriteLine("this is a farewell");
+>>>>>>> Stashed changes
             Console.WriteLine("running on .net 10.0 with C# 14.0 features");
         }
     }
